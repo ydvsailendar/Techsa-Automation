27b5b01b3c13a5741132b2612915361298f57cf5
ydvsailendar12 <shailendra@techstacksolutions.com>
2018-12-24 19:58:44 +0545

    contact and clients words

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,92 @@
 import React, { Component } from "react";
 
 export default class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1
+    }
+    this.getBody = this.getBody.bind(this);
+  }
+  componentDidMount() {
+    setInterval(this.getBody, 3000);
+  }
+
+  getBody() {
+    (this.state.count === 4) ?
+      this.setState({ count: 1 }) :
+      this.setState({ count: this.state.count + 1 });
+  }
+
+  getContainer() {
+    switch (this.state.count) {
+      case 1:
+        return (
+          <div>
+            <h5 className="grey-text text-darken-4">Laxmi Industry</h5>
+            <p className="grey-text text-darken-4">We have hired Techsa few times and every time we have been more then satisfied with the services thet provide.</p>
+          </div>
+        )
+      case 2:
+        return (
+          <div>
+            <h5 className="grey-text text-darken-4">Ma Durga Industry</h5>
+            <p className="grey-text text-darken-4">Techsa has been that one team which has always provided us the best timely solutions.</p>
+          </div>
+        )
+      case 3:
+        return (
+          <div>
+            <h5 className="grey-text text-darken-4">Hunger Industrial</h5>
+            <p className="grey-text text-darken-4">Techsa Automation has been the best service provider so far for us.</p>
+          </div>
+        )
+      case 4:
+        return (
+          <div>
+            <h5 className="grey-text text-darken-4">Dantey Industry</h5>
+            <p className="grey-text text-darken-4">Techsa Automation has made us trust the online service provider with this quick and timely service.</p>
+          </div>
+        )
+      default:
+        return (
+          null
+        )
+    }
+  }
+
   render() {
     return (
       <footer className="page-footer teal">
         <div className="container">
           <div className="row">
             <div className="col l6 s12">
-              <h5 className="white-text">Footer Content</h5>
-              <p className="grey-text text-lighten-4">
-                You can use rows and columns here to organize your footer
-                content.
-              </p>
+              <h5 className="white-text">Hear what our clients says about us</h5>
+              <div>
+                {this.getContainer()}
+              </div>
             </div>
             <div className="col l4 offset-l2 s12">
-              <h5 className="white-text">Links</h5>
+              <h5 className="white-text">Social Links</h5>
               <ul>
                 <li>
                   <a className="grey-text text-lighten-3" href="#!">
-                    Link 1
+                    <img src={require('../images/instagram.png')} alt="Instagram" />
                   </a>
                 </li>
                 <li>
                   <a className="grey-text text-lighten-3" href="#!">
-                    Link 2
+                    <img src={require('../images/twitter.png')} alt="Twitter" />
                   </a>
                 </li>
                 <li>
                   <a className="grey-text text-lighten-3" href="#!">
-                    Link 3
+                    <img src={require('../images/facebook.png')} alt="Facebook" />
                   </a>
                 </li>
                 <li>
                   <a className="grey-text text-lighten-3" href="#!">
-                    Link 4
+                    <img src={require('../images/website.png')} alt="Website" />
                   </a>
                 </li>
               </ul>
@@ -44,7 +97,7 @@ export default class Footer extends Component {
           <div className="container">
             © 2014 Copyright Techsa Industrial and Automations
             <a className="grey-text text-lighten-4 right" href="https://ydvsailendar.github.io/">
-              Designer & Developer ydvsailendar  
+              Designer & Developer ydvsailendar
             </a>
           </div>
         </div>
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -6,7 +6,7 @@ export default class ContactUs extends Component {
       <div className="row">
         <h4 className="header">About Us & Our Team</h4>
         <div className="divider"></div>
-        <div className="col s12 m7 l6">
+        <div className="col s12 m6 l6">
           <div className="card horizontal small">
             <div className="card-image">
               <img src={require("../images/roshan.jpeg")} alt="Engineer" />
@@ -14,24 +14,19 @@ export default class ContactUs extends Component {
             <div className="card-stacked">
               <div className="card-content">
                 <h4>Roshan Yadav</h4>
-                <span style={{ fontSize: "12px" }}>
-                  Address: Partappur-9 Nawalparasi-Nepal
-                </span>
-                <span style={{ fontSize: "13px" }}>
-                  <p>Email: frank.ros@gmail.com</p>
-                  <p>Phone: 9826439343</p>
-                </span>
                 <div className="divider" />
                 <p>
-                  Hello. I'm Roshan Yadav Founder and one of the lead engineer
-                  of the team. I have been working in this field for around 3
-                  years.
+                  Experienced Automation Engineer with a demonstrated history of working in the
+                  industrial automation industry. Skilled in instrumentation,
+                  PLC Programming, AutoCAD, Matlab, Microsoft Word, Engineering, and C++.
+                  Strong engineering professional with a Bachelor of Technology (B.Tech.)
+                  focused in Electrical, Electronics & Communications Engineering from Lovely Professional University.
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="col s12 m7 l6">
+        <div className="col s12 m6 l6">
           <div className="card horizontal small">
             <div className="card-image">
               <img src={require("../images/shailesh.jpg")} alt="Engineer" />
@@ -39,18 +34,12 @@ export default class ContactUs extends Component {
             <div className="card-stacked">
               <div className="card-content">
                 <h4>Shailesh Yadav</h4>
-                <span style={{ fontSize: "12px" }}>
-                  Address: Partappur-9 Nawalparasi-Nepal
-                </span>
-                <span style={{ fontSize: "13px" }}>
-                  <p>Email: ydvsailesh1994@gmail.com</p>
-                  <p>Phone: 9849121736</p>
-                </span>
                 <div className="divider" />
                 <p>
-                  Hello. I'm Shailesh Yadav Founder and one of the lead engineer
-                  of the team. I have been working in this field for around 4
-                  years.
+                  Experienced Engineer with a demonstrated history of working in the
+                  industry. Skilled in instrumentation, PLC Programming, AutoCAD, Matlab, Microsoft Word, Engineering, and C++.
+                  Strong engineering professional with a Bachelor of Technology (B.Tech.)
+                  focused in Electrical, Electronics & Communications Engineering from LPU.
                 </p>
               </div>
             </div>
